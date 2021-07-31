7d834b9244f956d0063b68b24fab415cb52c14dd
Adamantium <adamantiumhack@gmail.com>
2021-07-31 20:54:27 +0300

    init bsod

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![allow(dead_code, non_camel_case_types, non_snake_case, non_upper_case_globals)]
+
+use {
+    winapi::shared::ntdef::NTSTATUS,
+    winapi::shared::ntdef::ULONG,
+    winapi::shared::ntdef::BOOLEAN,
+    winapi::shared::ntdef::PBOOLEAN,
+    winapi::shared::ntdef::LONG,
+    winapi::shared::basetsd::PULONG_PTR,
+    winapi::shared::ntdef::PULONG,
+    winapi::shared::ntstatus::STATUS_ASSERTION_FAILURE
+};
+
+
+#[link(name = "ntdll")]
+extern {
+  fn RtlAdjustPrivilege(Privilege: ULONG, Enable: BOOLEAN, CurrentThread: BOOLEAN , OldValue: PBOOLEAN) -> NTSTATUS ;
+  fn NtRaiseHardError(ErrorStatus: LONG, NumberOfParameters: ULONG, UnicodeStringParameterMask: ULONG, Parameters: PULONG_PTR, ValidResponseOptions: ULONG, Response: PULONG) -> NTSTATUS; 
+}
+
+// cargo run --release --target x86_64-pc-windows-msvc
+fn main (){
+    unsafe {
+        println!("hello");
+        let mut b : BOOLEAN = 1;
+        let mut response : ULONG = 0 ;
+        RtlAdjustPrivilege(19, 0, 0, &mut b);
+        NtRaiseHardError(STATUS_ASSERTION_FAILURE,0,0,0 as *mut _,6, &mut response);
+        println!("{} {} ",b,response );
+    }
+}
